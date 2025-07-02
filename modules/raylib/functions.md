55285a26297f770dd9b444df8ccdbd94b5eb1c2a
Huseyn Aghayev <huseyn@seallang.org>
2025-07-02 10:46:52 +0400

    raylib module

---
+++ modules/raylib/functions.md
@@ -0,0 +1,96 @@
+# Seal Raylib Binding
+## Functions
+
+### Window-related
+- [x] init_window(w, h, title)
+- [x] close_window()
+- [x] window_should_close()
+- [ ] set_window_state(flags)
+
+### Cursor-related
+- [ ] ShowCursor(void);
+- [ ] HideCursor(void);
+- [ ] IsCursorHidden(void);
+- [ ] EnableCursor(void);
+- [ ] DisableCursor(void);
+- [ ] IsCursorOnScreen(void);
+
+### Drawing-related
+- [x] clear_background(r, g, b)
+- [x] begin_drawing()
+- [x] end_drawing()
+- [ ] BeginMode2D(Camera2D camera);
+- [ ] EndMode2D(void);
+- [ ] BeginMode3D(Camera3D camera);
+- [ ] EndMode3D(void);
+- [ ] BeginShaderMode(Shader shader);
+- [ ] EndShaderMode(void);
+- [ ] BeginScissorMode(int x, int y, int width, int height);
+- [ ] EndScissorMode(void);
+
+### Timing-related functions
+- [x] set_fps(fps) => SetTargetFPS(int fps);
+- [x] delta_time() => GetFrameTime(void);
+- [ ] GetTime(void);
+- [x] get_fps()
+
+### Input-related functions: keyboard
+- [x] is_key_pressed(key)
+- [ ] IsKeyPressedRepeat(int key);
+- [x] is_key_down(key)
+- [x] is_key_released(key)
+- [x] is_key_up(key);
+- [ ] GetKeyPressed(void);
+- [ ] GetCharPressed(void);
+- [x] set_exit_key(key)
+
+### Input-related functions: mouse
+- [x] is_mouse_pressed(button)
+- [x] is_mouse_down(button)
+- [x] is_mouse_released(button)
+- [x] is_mouse_up(button)
+- [x] mouse_x()
+- [x] mouse_y()
+- [ ] GetMouseDelta(void);
+- [ ] SetMousePosition(int x, int y);
+- [ ] SetMouseOffset(int offsetX, int offsetY);
+- [ ] SetMouseScale(float scaleX, float scaleY);
+- [ ] GetMouseWheelMove(void);
+- [ ] SetMouseCursor(int cursor);
+
+### Basic shapes drawing functions
+- [ ] DrawLine(int startPosX, int startPosY, int endPosX, int endPosY, Color color);                // Draw a line
+- [ ] DrawLineV(Vector2 startPos, Vector2 endPos, Color color);                                     // Draw a line (using gl lines)
+- [ ] DrawLineEx(Vector2 startPos, Vector2 endPos, float thick, Color color);                       // Draw a line (using triangles/quads)
+- [ ] DrawLineStrip(const Vector2 *points, int pointCount, Color color);                            // Draw lines sequence (using gl lines)
+- [ ] DrawLineBezier(Vector2 startPos, Vector2 endPos, float thick, Color color);                   // Draw line segment cubic-bezier in-out interpolation
+- [ ] DrawCircle(int centerX, int centerY, float radius, Color color);                              // Draw a color-filled circle
+- [ ] DrawCircleSector(Vector2 center, float radius, float startAngle, float endAngle, int segments, Color color);      // Draw a piece of a circle
+- [ ] DrawCircleSectorLines(Vector2 center, float radius, float startAngle, float endAngle, int segments, Color color); // Draw circle sector outline
+- [ ] DrawCircleGradient(int centerX, int centerY, float radius, Color inner, Color outer);         // Draw a gradient-filled circle
+- [ ] DrawCircleV(Vector2 center, float radius, Color color);                                       // Draw a color-filled circle (Vector version)
+- [ ] DrawCircleLines(int centerX, int centerY, float radius, Color color);                         // Draw circle outline
+- [ ] DrawCircleLinesV(Vector2 center, float radius, Color color);                                  // Draw circle outline (Vector version)
+- [ ] DrawEllipse(int centerX, int centerY, float radiusH, float radiusV, Color color);             // Draw ellipse
+- [ ] DrawEllipseLines(int centerX, int centerY, float radiusH, float radiusV, Color color);        // Draw ellipse outline
+- [ ] DrawRing(Vector2 center, float innerRadius, float outerRadius, float startAngle, float endAngle, int segments, Color color); // Draw ring
+- [ ] DrawRingLines(Vector2 center, float innerRadius, float outerRadius, float startAngle, float endAngle, int segments, Color color);    // Draw ring outline
+- [x] draw_rectangle(x, y, w, h, r, g, b, a)
+- [ ] DrawRectangleV(Vector2 position, Vector2 size, Color color);                                  // Draw a color-filled rectangle (Vector version)
+- [ ] DrawRectangleRec(Rectangle rec, Color color);                                                 // Draw a color-filled rectangle
+- [ ] DrawRectanglePro(Rectangle rec, Vector2 origin, float rotation, Color color);                 // Draw a color-filled rectangle with pro parameters
+- [ ] DrawRectangleGradientV(int posX, int posY, int width, int height, Color top, Color bottom);   // Draw a vertical-gradient-filled rectangle
+- [ ] DrawRectangleGradientH(int posX, int posY, int width, int height, Color left, Color right);   // Draw a horizontal-gradient-filled rectangle
+- [ ] DrawRectangleGradientEx(Rectangle rec, Color topLeft, Color bottomLeft, Color topRight, Color bottomRight); // Draw a gradient-filled rectangle with custom vertex colors
+- [ ] DrawRectangleLines(int posX, int posY, int width, int height, Color color);                   // Draw rectangle outline
+- [ ] DrawRectangleLinesEx(Rectangle rec, float lineThick, Color color);                            // Draw rectangle outline with extended parameters
+- [ ] DrawRectangleRounded(Rectangle rec, float roundness, int segments, Color color);              // Draw rectangle with rounded edges
+- [ ] DrawRectangleRoundedLines(Rectangle rec, float roundness, int segments, Color color);         // Draw rectangle lines with rounded edges
+- [ ] DrawRectangleRoundedLinesEx(Rectangle rec, float roundness, int segments, float lineThick, Color color); // Draw rectangle with rounded edges outline
+- [ ] DrawTriangle(Vector2 v1, Vector2 v2, Vector2 v3, Color color);                                // Draw a color-filled triangle (vertex in counter-clockwise order!)
+- [ ] DrawTriangleLines(Vector2 v1, Vector2 v2, Vector2 v3, Color color);                           // Draw triangle outline (vertex in counter-clockwise order!)
+- [ ] DrawTriangleFan(const Vector2 *points, int pointCount, Color color);                          // Draw a triangle fan defined by points (first vertex is the center)
+- [ ] DrawTriangleStrip(const Vector2 *points, int pointCount, Color color);                        // Draw a triangle strip defined by points
+- [ ] DrawPoly(Vector2 center, int sides, float radius, float rotation, Color color);               // Draw a regular polygon (Vector version)
+- [ ] DrawPolyLines(Vector2 center, int sides, float radius, float rotation, Color color);          // Draw a polygon outline of n sides
+- [ ] DrawPolyLinesEx(Vector2 center, int sides, float radius, float rotation, float lineThick, Color color); // Draw a polygon outline of n sides with extended parameters
---
+++ modules/sealraylib.c
@@ -0,0 +1,261 @@
+#include <seal.h>
+#include <raylib.h>
+
+#define color(r, g, b, a) ((Color) { r, g, b, a })
+
+static const char *MOD_NAME = "raylib";
+
+
+/* Window-related functions */
+seal_value __seal_raylib_init_window(seal_byte argc, seal_value *argv)
+{
+  static const char *FUNC_NAME = "init_window";
+
+  seal_value w, h, title;
+  seal_parse_args(MOD_NAME, FUNC_NAME, argc, argv, 3, PARAM_TYPES(SEAL_NUMBER, SEAL_NUMBER, SEAL_STRING), &w, &h, &title);
+
+  InitWindow(AS_NUM(w), AS_NUM(h), AS_STRING(title));
+
+  return SEAL_VALUE_NULL;
+}
+seal_value __seal_raylib_close_window(seal_byte argc, seal_value *argv)
+{
+  static const char *FUNC_NAME = "close_window";
+
+  seal_parse_args(MOD_NAME, FUNC_NAME, argc, argv, 0, NULL);
+
+  CloseWindow();
+
+  return SEAL_VALUE_NULL;
+}
+seal_value __seal_raylib_window_should_close(seal_byte argc, seal_value *argv)
+{
+  static const char *FUNC_NAME = "window_should_close";
+
+  seal_parse_args(MOD_NAME, FUNC_NAME, argc, argv, 0, NULL);
+
+  return SEAL_VALUE_BOOL(WindowShouldClose());
+}
+
+/* Drawing-related functions */
+seal_value __seal_raylib_clear_background(seal_byte argc, seal_value *argv)
+{
+  static const char *FUNC_NAME = "clear_background";
+
+  seal_value r, g, b;
+  seal_parse_args(MOD_NAME, FUNC_NAME, argc, argv, 3, PARAM_TYPES(SEAL_NUMBER, SEAL_NUMBER, SEAL_NUMBER), &r, &g, &b);
+
+  ClearBackground((Color) { AS_NUM(r), AS_NUM(g), AS_NUM(b) });
+
+  return SEAL_VALUE_NULL;
+}
+seal_value __seal_raylib_begin_drawing(seal_byte argc, seal_value *argv)
+{
+  static const char *FUNC_NAME = "begin_drawing";
+
+  seal_parse_args(MOD_NAME, FUNC_NAME, argc, argv, 0, NULL);
+
+  BeginDrawing();
+
+  return SEAL_VALUE_NULL;
+}
+seal_value __seal_raylib_end_drawing(seal_byte argc, seal_value *argv)
+{
+  static const char *FUNC_NAME = "end_drawing";
+
+  seal_parse_args(MOD_NAME, FUNC_NAME, argc, argv, 0, NULL);
+
+  EndDrawing();
+
+  return SEAL_VALUE_NULL;
+}
+
+/* Timing-related functions */
+seal_value __seal_raylib_set_fps(seal_byte argc, seal_value *argv)
+{
+  static const char *FUNC_NAME = "set_fps";
+
+  seal_value fps;
+  seal_parse_args(MOD_NAME, FUNC_NAME, argc, argv, 1, PARAM_TYPES(SEAL_INT), &fps);
+
+  SetTargetFPS(AS_INT(fps));
+
+  return SEAL_VALUE_NULL;
+}
+seal_value __seal_raylib_delta_time(seal_byte argc, seal_value *argv)
+{
+  static const char *FUNC_NAME = "delta_time";
+
+  seal_parse_args(MOD_NAME, FUNC_NAME, argc, argv, 0, NULL);
+
+  return SEAL_VALUE_FLOAT(GetFrameTime());
+}
+seal_value __seal_raylib_get_fps(seal_byte argc, seal_value *argv)
+{
+  static const char *FUNC_NAME = "get_fps";
+
+  seal_parse_args(MOD_NAME, FUNC_NAME, argc, argv, 0, NULL);
+
+  return SEAL_VALUE_INT(GetFPS());
+}
+
+/* Input-related functions: keyboard */
+seal_value __seal_raylib_is_key_pressed(seal_byte argc, seal_value *argv)
+{
+  static const char *FUNC_NAME = "is_key_pressed";
+
+  seal_value key;
+  seal_parse_args(MOD_NAME, FUNC_NAME, argc, argv, 1, PARAM_TYPES(SEAL_INT), &key);
+
+  return SEAL_VALUE_BOOL(IsKeyPressed(AS_INT(key)));
+}
+seal_value __seal_raylib_is_key_down(seal_byte argc, seal_value *argv)
+{
+  static const char *FUNC_NAME = "is_key_down";
+
+  seal_value key;
+  seal_parse_args(MOD_NAME, FUNC_NAME, argc, argv, 1, PARAM_TYPES(SEAL_INT), &key);
+
+  return SEAL_VALUE_BOOL(IsKeyDown(AS_INT(key)));
+}
+seal_value __seal_raylib_is_key_released(seal_byte argc, seal_value *argv)
+{
+  static const char *FUNC_NAME = "is_key_released";
+
+  seal_value key;
+  seal_parse_args(MOD_NAME, FUNC_NAME, argc, argv, 1, PARAM_TYPES(SEAL_INT), &key);
+
+  return SEAL_VALUE_BOOL(IsKeyReleased(AS_INT(key)));
+}
+seal_value __seal_raylib_is_key_up(seal_byte argc, seal_value *argv)
+{
+  static const char *FUNC_NAME = "is_key_up";
+
+  seal_value key;
+  seal_parse_args(MOD_NAME, FUNC_NAME, argc, argv, 1, PARAM_TYPES(SEAL_INT), &key);
+
+  return SEAL_VALUE_BOOL(IsKeyUp(AS_INT(key)));
+}
+seal_value __seal_raylib_set_exit_key(seal_byte argc, seal_value *argv)
+{
+  static const char *FUNC_NAME = "set_exit_key";
+
+  seal_value key;
+  seal_parse_args(MOD_NAME, FUNC_NAME, argc, argv, 1, PARAM_TYPES(SEAL_INT), &key);
+
+  SetExitKey(AS_INT(key));
+
+  return SEAL_VALUE_NULL;
+}
+
+/* Input-related functions: mouse */
+seal_value __seal_raylib_is_mouse_pressed(seal_byte argc, seal_value *argv)
+{
+  static const char *FUNC_NAME = "is_mouse_pressed";
+
+  seal_value button;
+  seal_parse_args(MOD_NAME, FUNC_NAME, argc, argv, 1, PARAM_TYPES(SEAL_INT), &button);
+
+  return SEAL_VALUE_BOOL(IsMouseButtonPressed(AS_INT(button)));
+}
+seal_value __seal_raylib_is_mouse_down(seal_byte argc, seal_value *argv)
+{
+  static const char *FUNC_NAME = "is_mouse_down";
+
+  seal_value button;
+  seal_parse_args(MOD_NAME, FUNC_NAME, argc, argv, 1, PARAM_TYPES(SEAL_INT), &button);
+
+  return SEAL_VALUE_BOOL(IsMouseButtonDown(AS_INT(button)));
+}
+seal_value __seal_raylib_is_mouse_released(seal_byte argc, seal_value *argv)
+{
+  static const char *FUNC_NAME = "is_mouse_released";
+
+  seal_value button;
+  seal_parse_args(MOD_NAME, FUNC_NAME, argc, argv, 1, PARAM_TYPES(SEAL_INT), &button);
+
+  return SEAL_VALUE_BOOL(IsMouseButtonReleased(AS_INT(button)));
+}
+seal_value __seal_raylib_is_mouse_up(seal_byte argc, seal_value *argv)
+{
+  static const char *FUNC_NAME = "is_mouse_up";
+
+  seal_value button;
+  seal_parse_args(MOD_NAME, FUNC_NAME, argc, argv, 1, PARAM_TYPES(SEAL_INT), &button);
+
+  return SEAL_VALUE_BOOL(IsMouseButtonUp(AS_INT(button)));
+}
+seal_value __seal_raylib_mouse_x(seal_byte argc, seal_value *argv)
+{
+  static const char *FUNC_NAME = "mouse_x";
+
+  seal_parse_args(MOD_NAME, FUNC_NAME, argc, argv, 0, NULL);
+
+  return SEAL_VALUE_INT(GetMouseX());
+}
+seal_value __seal_raylib_mouse_y(seal_byte argc, seal_value *argv)
+{
+  static const char *FUNC_NAME = "mouse_y";
+
+  seal_parse_args(MOD_NAME, FUNC_NAME, argc, argv, 0, NULL);
+
+  return SEAL_VALUE_INT(GetMouseY());
+}
+
+/* Basic shapes drawing functions */
+seal_value __seal_raylib_draw_rectangle(seal_byte argc, seal_value *argv)
+{
+  static const char *FUNC_NAME = "draw_rectangle";
+
+  seal_value x, y, w, h, r, g, b, a;
+  seal_parse_args(MOD_NAME,
+                  FUNC_NAME,
+                  argc,
+                  argv,
+                  8,
+                  PARAM_TYPES(SEAL_NUMBER, SEAL_NUMBER, SEAL_NUMBER, SEAL_NUMBER, SEAL_NUMBER, SEAL_NUMBER, SEAL_NUMBER, SEAL_NUMBER),
+                  &x, &y, &w, &h, &r, &g, &b, &a);
+
+  DrawRectangle(AS_NUM(x), AS_NUM(y), AS_NUM(w), AS_NUM(h), color(AS_NUM(r), AS_NUM(g), AS_NUM(b), AS_NUM(a)));
+
+  return SEAL_VALUE_NULL;
+}
+seal_value seal_init_mod()
+{
+  seal_value mod = {
+    .type = SEAL_MOD,
+    .as.mod = SEAL_CALLOC(1, sizeof(struct seal_module))
+  };
+  AS_MOD(mod)->name = MOD_NAME;
+  AS_MOD(mod)->globals = SEAL_CALLOC(1, sizeof(hashmap_t));
+  hashmap_init(AS_MOD(mod)->globals, 256);
+
+  MOD_REGISTER_FUNC(mod, __seal_raylib_init_window, "init_window", 3, false);
+  MOD_REGISTER_FUNC(mod, __seal_raylib_close_window, "close_window", 0, false);
+  MOD_REGISTER_FUNC(mod, __seal_raylib_window_should_close, "window_should_close", 0, false);
+
+  MOD_REGISTER_FUNC(mod, __seal_raylib_clear_background, "clear_background", 3, false);
+  MOD_REGISTER_FUNC(mod, __seal_raylib_begin_drawing, "begin_drawing", 0, false);
+  MOD_REGISTER_FUNC(mod, __seal_raylib_end_drawing, "end_drawing", 0, false);
+
+  MOD_REGISTER_FUNC(mod, __seal_raylib_set_fps, "set_fps", 1, false);
+  MOD_REGISTER_FUNC(mod, __seal_raylib_delta_time, "delta_time", 0, false);
+  MOD_REGISTER_FUNC(mod, __seal_raylib_get_fps, "get_fps", 0, false);
+
+  MOD_REGISTER_FUNC(mod, __seal_raylib_is_key_pressed, "is_key_pressed", 1, false);
+  MOD_REGISTER_FUNC(mod, __seal_raylib_is_key_down, "is_key_down", 1, false);
+  MOD_REGISTER_FUNC(mod, __seal_raylib_is_key_released, "is_key_released", 1, false);
+  MOD_REGISTER_FUNC(mod, __seal_raylib_is_key_up, "is_key_up", 1, false);
+  MOD_REGISTER_FUNC(mod, __seal_raylib_set_exit_key, "set_exit_key", 1, false);
+
+  MOD_REGISTER_FUNC(mod, __seal_raylib_is_mouse_pressed, "is_mouse_pressed", 1, false);
+  MOD_REGISTER_FUNC(mod, __seal_raylib_is_mouse_down, "is_mouse_down", 1, false);
+  MOD_REGISTER_FUNC(mod, __seal_raylib_is_mouse_released, "is_mouse_released", 1, false);
+  MOD_REGISTER_FUNC(mod, __seal_raylib_is_mouse_up, "is_mouse_up", 1, false);
+  MOD_REGISTER_FUNC(mod, __seal_raylib_mouse_x, "mouse_x", 0, false);
+  MOD_REGISTER_FUNC(mod, __seal_raylib_mouse_y, "mouse_y", 0, false);
+
+  MOD_REGISTER_FUNC(mod, __seal_raylib_draw_rectangle, "draw_rectangle", 8, false);
+
+  return mod;
+}
