979c4219e68ea29b5226a642385cfba8ab4193c4
Huseyn Aghayev <huseyn@seallang.org>
2025-08-19 14:07:26 +0400

    build scripts options

--- modules/buildmod.sh
+++ modules/buildmod.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
+OS="linux"
 REQ_FILES="../src/moddef.c"
 INC_FLAGS="-I ./ -I ../src"
 MOD=$1
 shift
 FLAGS=""
 while [[ $# -ne 0 ]]; do
-    FLAGS="$FLAGS $1"
+    if [[ $1 == "--windows" ]]; then
+        OS="windows"
+    else
+        FLAGS="$FLAGS $1"
+    fi
     shift
 done
 
-gcc -fPIC -shared -o "$MOD.so" "$MOD.c" $REQ_FILES $INC_FLAGS $FLAGS
-x86_64-w64-mingw32-gcc -shared -o "$MOD.dll" "$MOD.c" $REQ_FILES $INC_FLAGS $FLAGS
+echo "compiling for $OS..."
+if [[ $OS == "linux" ]]; then
+    gcc -fPIC -shared -o "$MOD.so" "$MOD.c" $REQ_FILES $INC_FLAGS $FLAGS
+else
+    x86_64-w64-mingw32-gcc -shared -o "$MOD.dll" "$MOD.c" $REQ_FILES $INC_FLAGS $FLAGS
+fi
