3d5ec5a0b38974adfa4ab492b344891bf13e5407
Huseyn Aghayev <huseyn@seallang.org>
2025-09-20 21:19:33 +0400

    ast defined

---
+++ src/ast.h
@@ -0,0 +1,203 @@
+#ifndef AST_H
+#define AST_H
+
+
+#include "sealconf.h"
+
+
+enum {
+    AST_NOP,  /* indicates end of node (no operation) */
+    /* values */
+    AST_NULL, /* null */
+    AST_INT, AST_FLOAT, AST_BOOL, /* numerics */
+    AST_STRING, AST_LIST, AST_MAP, /* references */
+    AST_FUNC_DEF, /* function definition */
+    AST_NAME, /* name */
+    AST_FUNC_CALL, /* function call (a()) */
+    AST_METH_CALL, /* method call (a->b) */
+    AST_INDEX, /* a[0] */
+    AST_FIELD, /* a.b */
+    /* compound statements */
+    AST_COMP, /* compound (program) */
+    AST_IF, AST_ELSE, /* conditions */
+    AST_WHILE, AST_DOWHILE, AST_FOR, /* loops */
+    /* simple statements */
+    AST_SKIP, AST_STOP, AST_RETURN, AST_INCLUDE,
+
+    /* operations (precedence goes higher to lower) */
+
+    AST_UNARY,   /* left-to-right
+                    ++ -- (postfix increment and decrement)
+                  -------------------
+                    right-to-left
+                    ++ -- (prefix increment and decrement)
+                    + -  (unary plus and minus)
+                    ! not ~  (logical NOT and bitwise NOT)
+                 */
+
+    AST_BINARY,  /* left-to-right
+                    * / %
+                    + -
+                    << >>
+                    < <=
+                    > >=
+                    == !=
+                    &  (bitwise AND)
+                    ^  (XOR)
+                    |  (bitwise OR)
+                 */
+
+    AST_LOGBIN,  /* left-to-right
+                    and  (logical AND)
+                    or   (logical OR)
+                 */
+
+    AST_TERNARY, /* right-to-left
+                    if <expr> then <expr> else <expr>
+                 */
+
+    AST_ASSIGN,  /* right-to-left
+                    = *= /= %= += -= <<= >>= &= ^= |=
+                 */
+};
+
+enum { /* immediate operations (for parsing) */
+    /* unaries */
+    IMOP_POSTFIX_INC, /* a++ */
+    IMOP_POSTFIX_DEC, /* a-- */
+    IMOP_PREFIX_INC,  /* --b */
+    IMOP_PREFIX_DEC,  /* --a */
+    /* unary plus is not defined,
+     * because writing +a does not make any sense
+     */
+    IMOP_UNARY_MINUS, /* -a */
+    IMOP_LOGICAL_NOT, /* !a (or) not a (even combined: !not!!not a) */
+    IMOP_BITWISE_NOT, /* ~a */
+
+    /* binaries */
+    IMOP_MUL, IMOP_DIV, IMOP_MOD, /* a * b / c % d */
+    IMOP_ADD, IMOP_SUB, /* a + b - c */
+    IMOP_SHL, IMOP_SHR, /* a << b >> c */
+    IMOP_LT, IMOP_LE,   /* a < b <= c */
+    IMOP_GT, IMOP_GE,   /* a > b >= c */
+    IMOP_EQ, IMOP_NE,   /* a == b != c */
+    IMOP_BITWISE_AND,   /* a & b */
+    IMOP_XOR,           /* a ^ b */
+    IMOP_BITWISE_OR,    /* a | b */
+
+    /* logical binaries */
+    IMOP_AND, /* a and b */
+    IMOP_OR,  /* a or b */
+
+    /* assignments */
+    IMOP_ASSIGN, /* = */
+    IMOP_MUL_ASSIGN, IMOP_DIV_ASSIGN, IMOP_MOD_ASSIGN,
+    IMOP_ADD_ASSIGN, IMOP_SUB_ASSIGN,
+    IMOP_SHL_ASSIGN, IMOP_SHR_ASSIGN,
+    IMOP_AND_ASSIGN, IMOP_XOR_ASSIGN, IMOP_OR_ASSIGN,
+};
+
+struct ast {
+    int type;
+    union {
+        /* values */
+        seal_int    i; /* integer */
+        seal_float  f; /* floating point */
+        seal_bool   b; /* boolean */
+        const char *s; /* used for both string and name */
+
+        struct { /* list (array) */
+            struct ast **items;
+            size_t size;
+        } l;
+
+        struct { /* map (hashmap) */
+            const char **keys;
+            struct ast **vals;
+            size_t size;
+        } m;
+
+        struct { /* function definition */
+            const char *name; /* if NULL ptr, then anonymous */
+            const char **params; /* parameter names */
+            size_t psize;
+            struct ast *body;
+        } func;
+
+        struct { /* used for both function and method calls */
+            struct ast *f;
+            struct ast **argv;
+            size_t argc;
+        } call;
+
+       struct { /* subscript indexing */
+            struct ast *m;
+            struct ast *i;
+        } index;
+
+       struct { /* field indexing */
+            struct ast *m;
+            struct ast *f;
+        } field;
+
+       /* compound statements */
+       struct { /* compound statement */
+           struct ast **stmts;
+           size_t size;
+       } comp;
+
+       struct { /* if */
+           struct ast *cond;
+           struct ast *body;
+           struct ast *elsepart;
+           int haselse;
+       } ifstmt;
+
+       struct { /* else */
+           struct ast *body;
+       } elsestmt;
+
+       struct { /* used for both while and do-while statements */
+           struct ast *cond;
+           struct ast *body;
+       } whilestmt;
+
+       struct { /* for */
+           const char *it;
+           struct ast *ited;
+           struct ast *body;
+       } forstmt;
+
+       /* simple statements */
+       struct {
+           struct ast *val; /* if NULL ptr, then return null */
+       } retstmt;
+
+       struct {
+           const char *fname; /* file name */
+       } inclstmt;
+
+       /* operations */
+       struct {
+           int op;
+           struct ast *e;
+       } unary;
+
+       struct { /* used for both binary and logical binary expressions */
+           int op;
+           struct ast *l, *r;
+       } bin;
+
+       struct {
+           struct ast *c, *t, *e;
+       } ternary;
+
+       struct {
+           int op;
+           struct ast *var, *val;
+       } assign;
+    } as;
+};
+
+
+#endif /* AST_H */
