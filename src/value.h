c0e3333e73b82f88c590411f82366125a1671175
Huseyn Aghayev <huseyn@seallang.org>
2025-09-21 07:01:30 +0400

    type macros and hashmap free, remove functions protos

--- src/value.h
+++ src/value.h
@@ -7,14 +7,26 @@
 #include "sealconf.h"
 
 
+#define SEAL_TNULL    0
+#define SEAL_TBOOL    1
+#define SEAL_TINT     2
+#define SEAL_TFLOAT     3
+#define SEAL_TSTRING    4
+#define SEAL_TLIST   5
+#define SEAL_TMAP    6
+#define SEAL_TFUNCTION  7
+
+
 /* forward declarations */
 struct seal_string;
 struct seal_list;
 struct seal_hashmap;
+struct seal_func;
 
 
 struct seal_value {
     /* ... */
+    int type;
     union {
         seal_int       integer;
         seal_float     floating;
@@ -22,6 +34,7 @@ struct seal_value {
         struct seal_string  *string;
         struct seal_list    *list;
         struct seal_hashmap *map;
+        struct seal_func *func; /* prototype */
     } as;
 };
 
@@ -76,14 +89,41 @@ struct seal_hashmap {
 };
 
 struct seal_hashmap *hashmap_new(int cap, bool collect);
-struct h_entry *hashmap_searchlen(struct seal_hashmap *map, const char *key, int len);
-int hashmap_insert(struct seal_hashmap *map, const char *key, struct seal_value val);
-int hashmap_insert_e(struct seal_hashmap *map, struct h_entry *entry, const char *key, struct seal_value val);
+
+struct h_entry *hashmap_searchlen(struct seal_hashmap *map,
+                                  const char *key,
+                                  int len);
+
+int hashmap_insert(struct seal_hashmap *map,
+                   const char *key,
+                   struct seal_value val);
+
+int hashmap_insert_e(struct seal_hashmap *map,
+                     struct h_entry *entry,
+                     const char *key,
+                     struct seal_value val);
+
+int hashmap_remove(struct seal_hashmap *map, const char *key);
+int hashmap_free(struct seal_hashmap *map, bool free_key, bool collect);
 
 
 #define hashmap_Cnew(cap) hashmap_new(cap, true)
 #define hashmap_Nnew(cap) hashmap_new(cap, false)
 
 #define hashmap_search(map, key) hashmap_searchlen(map, key, -1)
+
+/* function */
+struct seal_func {
+    const char *name;  /* NULL ptr if anonymous */
+    seal_byte *body;   /* bytecode */
+    seal_byte psize;   /* number of parameters */
+    seal_byte locsize; /* local size */
+};
+
+struct seal_func *func_new(const char *name,
+                           seal_byte *body,
+                           seal_byte psize,
+                           seal_byte locsize);
+
 
 #endif /* VALUE_H */
