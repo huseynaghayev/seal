d341460a4c1e26a7bf9b07371376450b15fc8875
Huseyn Aghayev <huseyn@seallang.org>
2026-05-19 12:54:36 +0400

    change seal_* to math_*

--- src/sealmath.c
+++ src/sealmath.c
@@ -4,7 +4,7 @@
 #include <stdint.h> /* uintptr_t */
 #include "state.h" /* random_state */
 
-static void seal_abs(seal_state *S)
+static void math_abs(seal_state *S)
 {
     seal_checkargc(S, 1);
     if (seal_isint(S, 0)) {
@@ -16,115 +16,115 @@ static void seal_abs(seal_state *S)
     }
 }
 
-static void seal_sqrt(seal_state *S)
+static void math_sqrt(seal_state *S)
 {
     seal_checkargc(S, 1);
     seal_pushfloat(S, sqrt(seal_checknumber(S, 0)));
 }
 
-static void seal_pow(seal_state *S)
+static void math_pow(seal_state *S)
 {
     seal_checkargc(S, 2);
     seal_pushfloat(S, pow(seal_checknumber(S, 0), seal_checknumber(S, 1)));
 }
 
-static void seal_cbrt(seal_state *S)
+static void math_cbrt(seal_state *S)
 {
     seal_checkargc(S, 1);
     seal_pushfloat(S, cbrt(seal_checknumber(S, 0)));
 }
 
-static void seal_sin(seal_state *S)
+static void math_sin(seal_state *S)
 {
     seal_checkargc(S, 1);
     seal_pushfloat(S, sin(seal_checknumber(S, 0)));
 }
 
-static void seal_cos(seal_state *S)
+static void math_cos(seal_state *S)
 {
     seal_checkargc(S, 1);
     seal_pushfloat(S, cos(seal_checknumber(S, 0)));
 }
 
-static void seal_tan(seal_state *S)
+static void math_tan(seal_state *S)
 {
     seal_checkargc(S, 1);
     seal_pushfloat(S, tan(seal_checknumber(S, 0)));
 }
 
-static void seal_asin(seal_state *S)
+static void math_asin(seal_state *S)
 {
     seal_checkargc(S, 1);
     seal_pushfloat(S, asin(seal_checknumber(S, 0)));
 }
 
-static void seal_acos(seal_state *S)
+static void math_acos(seal_state *S)
 {
     seal_checkargc(S, 1);
     seal_pushfloat(S, acos(seal_checknumber(S, 0)));
 }
 
-static void seal_atan(seal_state *S)
+static void math_atan(seal_state *S)
 {
     seal_checkargc(S, 1);
     seal_pushfloat(S, atan(seal_checknumber(S, 0)));
 }
 
-static void seal_atan2(seal_state *S)
+static void math_atan2(seal_state *S)
 {
     seal_checkargc(S, 2);
     seal_pushfloat(S, atan2(seal_checknumber(S, 0), seal_checknumber(S, 1)));
 }
 
-static void seal_exp(seal_state *S)
+static void math_exp(seal_state *S)
 {
     seal_checkargc(S, 1);
     seal_pushfloat(S, exp(seal_checknumber(S, 0)));
 }
 
-static void seal_log(seal_state *S)
+static void math_log(seal_state *S)
 {
     seal_checkargc(S, 1);
     seal_pushfloat(S, log(seal_checknumber(S, 0)));
 }
 
-static void seal_log10(seal_state *S)
+static void math_log10(seal_state *S)
 {
     seal_checkargc(S, 1);
     seal_pushfloat(S, log10(seal_checknumber(S, 0)));
 }
 
-static void seal_log2(seal_state *S)
+static void math_log2(seal_state *S)
 {
     seal_checkargc(S, 1);
     seal_pushfloat(S, log2(seal_checknumber(S, 0)));
 }
 
-static void seal_floor(seal_state *S)
+static void math_floor(seal_state *S)
 {
     seal_checkargc(S, 1);
     seal_pushint(S, (seal_int)floor(seal_checknumber(S, 0)));
 }
 
-static void seal_ceil(seal_state *S)
+static void math_ceil(seal_state *S)
 {
     seal_checkargc(S, 1);
     seal_pushint(S, (seal_int)ceil(seal_checknumber(S, 0)));
 }
 
-static void seal_round(seal_state *S)
+static void math_round(seal_state *S)
 {
     seal_checkargc(S, 1);
     seal_pushint(S, (seal_int)round(seal_checknumber(S, 0)));
 }
 
-static void seal_trunc(seal_state *S)
+static void math_trunc(seal_state *S)
 {
     seal_checkargc(S, 1);
     seal_pushint(S, (seal_int)trunc(seal_checknumber(S, 0)));
 }
 
-static void seal_min(seal_state *S)
+static void math_min(seal_state *S)
 {
     seal_checkargcvar(S, 2);
     int n = seal_gettop(S);
@@ -148,7 +148,7 @@ static void seal_min(seal_state *S)
     }
 }
 
-static void seal_max(seal_state *S)
+static void math_max(seal_state *S)
 {
     seal_checkargcvar(S, 2);
     int n = seal_gettop(S);
@@ -172,25 +172,25 @@ static void seal_max(seal_state *S)
     }
 }
 
-static void seal_isnan(seal_state *S)
+static void math_isnan(seal_state *S)
 {
     seal_checkargc(S, 1);
     seal_pushbool(S, isnan(seal_checknumber(S, 0)));
 }
 
-static void seal_isinf(seal_state *S)
+static void math_isinf(seal_state *S)
 {
     seal_checkargc(S, 1);
     seal_pushbool(S, isinf(seal_checknumber(S, 0)));
 }
 
-static void seal_isfinite(seal_state *S)
+static void math_isfinite(seal_state *S)
 {
     seal_checkargc(S, 1);
     seal_pushbool(S, isfinite(seal_checknumber(S, 0)));
 }
 
-static void seal_random(seal_state *S)
+static void math_random(seal_state *S)
 {
     S->random_state = S->random_state * 1664525 + 1013904223;
     unsigned int r = S->random_state;
@@ -220,7 +220,7 @@ static void seal_random(seal_state *S)
     }
 }
 
-#define REG(name) { #name, seal_##name }
+#define REG(name) { #name, math_##name }
 
 static const seal_reg mathlib[] = {
     REG(abs),
