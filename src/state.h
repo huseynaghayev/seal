273b251750a78c1f089ffe39ae7c6eb6a680ce45
Huseyn Aghayev <huseyn@seallang.org>
2026-06-03 19:21:03 +0400

    remove state.h functions because of seal.h

--- src/state.h
+++ src/state.h
@@ -5,6 +5,7 @@
 #include "seal.h"
 #include "value.h"
 #include "lexer.h"
+#include "seal.h"
 #include <setjmp.h>
 
 #define SEAL_OK  0
@@ -63,95 +64,7 @@ typedef struct seal_state {
 } seal_state;
 
 
-seal_state *seal_state_new();
-void seal_state_free(seal_state *S);
 void seal_error(seal_state *S, int errln, const char *fmt, ...);
-void seal_throw(seal_state *S, const char *msg, ...);
-const char *seal_geterror(seal_state *S);
-
-int seal_dostring(seal_state *S, const char *str);
-int seal_dofile(seal_state *S, const char *file_name);
-int seal_call(seal_state *S, int argc);
-int seal_icall(seal_state *S, int argc);
-
-int seal_gettop(seal_state *S);
-void seal_movetop(seal_state *S, int offset);
-void seal_checkargcrange(seal_state *S, int min, int max);
-#define seal_checkargc(S, c) seal_checkargcrange(S, c, c)
-#define seal_checkargcmin(S, c) seal_checkargcrange(S, c, -1)
-#define seal_checkargcmax(S, c) seal_checkargcrange(S, -1, c)
-int seal_gettype(seal_state *S, int i);
-const char *seal_gettypename(seal_state *S, int i);
-
-/* values */
-seal_bool   seal_tobool(seal_state *S, int i);
-seal_int    seal_toint(seal_state *S, int i);
-seal_float  seal_tofloat(seal_state *S, int i);
-seal_float  seal_tonumber(seal_state *S, int i);
-const char *seal_tostring(seal_state *S, int i);
-void       *seal_touserdata(seal_state *S, int i);
-
-void        seal_checktype(seal_state *S, int i, int type);
-seal_bool   seal_checkbool(seal_state *S, int i);
-seal_int    seal_checkint(seal_state *S, int i);
-seal_float  seal_checkfloat(seal_state *S, int i);
-seal_float  seal_checknumber(seal_state *S, int i);
-const char *seal_checkstring(seal_state *S, int i);
-void       *seal_checkuserdata(seal_state *S, int i);
-
-/* push */
-
-void seal_pushidx(seal_state *S, int i);
-void seal_pushnull(seal_state *S);
-void seal_pushbool(seal_state *S, int b);
-void seal_pushint(seal_state *S, seal_int n);
-void seal_pushfloat(seal_state *S, seal_float f);
-void seal_pushstringx(seal_state *S, const char *str, int len, bool dup, bool is_const);
-#define seal_pushstring(S, str)  seal_pushstringx(S, str, -1, true, false)
-#define seal_pushlstring(S, str, l) seal_pushstringx(S, str, l, false, false)
-#define seal_pushstringn(S, str) seal_pushstringx(S, str, -1, false, false)
-#define seal_pushstringc(S, str) seal_pushstringx(S, str, -1, false, true)
-void seal_pushCfunc(seal_state *S, seal_Cfunction f);
-void seal_makelist(seal_state *S, int size);
-#define seal_newlist(S) seal_makelist(S, 0)
-void seal_makemap(seal_state *S, int size);
-#define seal_newmap(S) seal_makemap(S, 0)
-void seal_pushuserdata(seal_state *S, void *p);
-
-/* get */
-/* return 0 if it exists, 1 if not found (nothing is pushed) */
-int seal_getglobal(seal_state *S, const char *name); /* push value on top */
-int seal_getindex(seal_state *S, int i);
-/* return 0 if it exists
- * 1 if not found (null is pushed for now)
- * -1 if not map (null is pushed for now)
- */
-int seal_getfield(seal_state *S, int map_i, const char *key);
-
-/* set */
-/* return 0 if it existed before, 1 if new */
-int seal_setglobal(seal_state *S, const char *name); /* value is on top */
-int seal_setindex(seal_state *S, int list_i, int i);
-/* return 0 if it existed before
- * 1 if new
- * -1 if not map
- */
-int seal_setfield(seal_state *S, int map_i, const char *key);
-
-/* TODO: fix seal.h state.h same function declarations */
-/*
-typedef struct {
-    const char *name;
-    seal_Cfunction f;
-} seal_reg;
-
-#define seal_register(S, f, name) ( \
-    seal_pushCfunc(S, f), \
-    seal_setglobal(S, name) \
-)
-void seal_newlib(seal_state *S, const seal_reg *reg);
-*/
-
 void seal_gc(seal_state *S);
 
 #endif /* STATE_H */
